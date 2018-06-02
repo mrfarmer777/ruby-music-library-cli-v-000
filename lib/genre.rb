d3e6b2794b37c3be6fd4079cdd65399e44b0d9c5
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 03:43:13 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,34 @@
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@all=[]
+
+  def initialize(name)
+    @name=name    #initializes with a name
+  end
+
+  ##! How is this going to be generalizable?
+  def self.all  #expose the class variable @@all
+    @@all
+  end
+
+  def self.destroy_all #destroy all songs!
+    @@all.clear
+  end
+
+  def save
+    @@all<<self  #generalizable save method that requires the all method
+  end
+
+  def self.create(name) #just like initialize, but with save!
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+
+
+
+
+end
