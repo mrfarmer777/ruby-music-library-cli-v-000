b41d645f674c99c23634628a3437eea148a79b01
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 18:56:07 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -40,8 +40,9 @@ class MusicLibraryController
 
   def list_songs
     sorted=Song.all.sort_by {|song| song.name}
+    binding.pry
     sorted.each_with_index do |song,index|
-      binding.pry
+      
       puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     end
   end
