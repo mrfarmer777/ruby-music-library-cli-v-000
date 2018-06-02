1d5277cf7279f06b9c4513789d653028bd6cbb7a
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 05:26:31 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,6 +4,15 @@ module Concerns::Findable
     self.all.detect {|item| item.name==name}
   end
 
+  def find_or_create_by_name(name)
+    res=self.find_by_name(name)
+    if res==nil
+      self.create(name)
+    else
+      res
+    end
+  end
+
 
 
 
