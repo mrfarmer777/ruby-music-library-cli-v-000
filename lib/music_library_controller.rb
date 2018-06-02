12ebf187e199a3433b11b7d2daab81fe995a4b86
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 18:35:44 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,6 +14,11 @@ class MusicLibraryController
     puts "To list all of the genres in your library, enter 'list genres'."
     puts "To list all of the songs by a particular artist, enter 'list artist'."
     puts "To list all of the songs of a parrticular genre, enter 'list genre'."
+    puts "To play a song, enter 'play song'."
+    puts "To quit, type 'exit'."
+    puts "What would you like to do?"
+
+
     active=true
     while active
       resp = gets.strip
