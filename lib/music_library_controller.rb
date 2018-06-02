34993599c4d3325b4b1c3a48bf1e6e39ac11d199
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 14:45:40 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -10,8 +10,8 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of your songs, enter 'list artists'."
-    puts "To list all of your songs, enter 'list genres'."
+    puts "To list all of the artists in your library, enter 'list artists'."
+    puts "To list all of the genres in your library, enter 'list genres'."
     active=true
     while active
       resp = gets.strip
