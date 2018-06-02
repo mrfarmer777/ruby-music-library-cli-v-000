26d21bd164dd6c952507a10a588eed94a3fa8073
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 14:28:19 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController
   attr_accessor :path
 
-  def initialize(path="./db/mp3s/")
+  def initialize(path="./db/mp3s")
     @path=path
     importer=MusicImporter.new(@path)
   end
