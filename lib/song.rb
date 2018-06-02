6abe8d454e9c6cf0f8705ae0c0f4efee160879f2
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 03:40:23 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   end
 
   def save
-    Song.all<<self  #generalizable save method that requires the all method
+    @@all<<self  #generalizable save method that requires the all method
   end
 
   def self.create(name) #just like initialize, but with save!
