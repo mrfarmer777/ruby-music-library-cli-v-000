a8ede36018e66333a0aea6864efea546d1705fa2
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 06:09:08 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -8,7 +8,7 @@ class MusicImporter
 
   def files
     file_list=Dir[@path+"/*.mp3"]
-    file_list.collect {|file_name| file_name.sub(".mp3","")}
+    file_list.collect {|file_name| file_name.split("/mp3s/")[1].sub(".mp3","")}
   end
 
 end
