8e2cba6f9501010430601e6a40b7bfb3cbabf0b5
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 04:59:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -39,7 +39,7 @@ class Artist
     songs=self.songs
     if song.artist==nil
       song.artist=self
-      if !self.songs.include?(song)
+      if !songs.include?(song)
         @songs<<song
       end
     end
