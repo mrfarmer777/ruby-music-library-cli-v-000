cafad0554765a2a8f1a07a42cc4e1431e61b0131
Matt Farmer <mrfarmer777@gmail.com>
2018-06-02 14:27:55 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,3 +1,12 @@
 class MusicLibraryController
+  attr_accessor :path
 
+  def initialize(path="./db/mp3s/")
+    @path=path
+    importer=MusicImporter.new(@path)
+  end
+
+  def call
+    "welcome to the music library"
+  end
 end
